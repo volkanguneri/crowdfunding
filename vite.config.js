8c16f564162bcf55eb63af4cec6bf1ef3fad6bcb
Vocci <vocci[Chotmail.com>
2023-05-30 14:17:58 +0200

    vite image display configuration with env variables to make it dynamic

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/crowdfunding/",
+  base: process.env.BASE_URL || '/',
   plugins: [react()],
 })
