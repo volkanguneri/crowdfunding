21a09d3b9eef69f2f82a834b58ab74500a025b64
volkanguneri <volkanguneri@users.noreply.github.com>
2023-05-30 15:12:28 +0000

    deploy: 69ead66a2b607a4914d9c8dd23fbd723ade1cddb

--- assets/index-0fd2c2a9.js
+++ assets/index-0fd2c2a9.js
@@ -93,14 +93,14 @@ Error generating stack: `+o.message+`
     justify-content:center;
     align-items:top;
     gap:1.25em;
-    background:url('../../public/images/image-hero-mobile.jpg') top center/ cover no-repeat;
+    background:url('public/images/image-hero-mobile.jpg') top center/ cover no-repeat;
     margin-left:auto;
     margin-right:auto;
     color:white;
 
 
 @media screen and (min-width:700px) {
-    background:url('../../public/images/image-hero-desktop.jpg') top center/ cover no-repeat;
+    background:url('public/images/image-hero-desktop.jpg') top center/ cover no-repeat;
     width:100%;
     height:100%;
     padding-bottom:9.5em;
