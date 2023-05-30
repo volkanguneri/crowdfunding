a1bd5f65600c3fd15284b8ba1f55dd98dac81e3d
Vocci <vocci[Chotmail.com>
2023-05-30 19:14:40 +0200

    image display prob:em fixed by changing paths for ptoduction with /crowdfunding/

--- src/components/ThanksComponent.jsx
+++ src/components/ThanksComponent.jsx
@@ -5,7 +5,7 @@ function ThanksComponent({ handleGotItBtn }) {
 
     return ReactDom.createPortal(
         <StyledThanksComponent>
-            <img src="/crowdfunding/public/images/icon-check.svg" alt="check icon" />
+            <img src="/crowdfunding/images/icon-check.svg" alt="check icon" />
             <h2>Thanks for your support!</h2>
             <p>Your pledge brings us one step closer to sharing Mastercraft Bamboo Monitor Riser worldwide. You will get an email once our campaign is completed</p>
             <button onClick={handleGotItBtn}>Got it!</button>
