bba319b4ec748e1bf279058914302dfd9ede26c9
Vocci <vocci[Chotmail.com>
2023-05-30 14:29:16 +0200

    vite image conf 5

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: import.meta.env.BASE_URL || '/crowdfund/',
+  base: import.meta.env.BASE_URL || '/',
   plugins: [react()],
 })
