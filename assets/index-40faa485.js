569f513186df13a85caf0db8edcf5751a685c007
volkanguneri <volkanguneri@users.noreply.github.com>
2023-05-30 11:00:16 +0000

    deploy: 260cfded40759703094b002ca028da44604dc183

--- assets/index-40faa485.js
+++ assets/index-40faa485.js
@@ -93,7 +93,7 @@ Error generating stack: `+o.message+`
     justify-content:center;
     align-items:top;
     gap:1.25em;
-    background:url('/public/images/image-hero-mobile.jpg') top center/ cover no-repeat;
+    background:url('./public/images/image-hero-mobile.jpg') top center/ cover no-repeat;
     margin-left:auto;
     margin-right:auto;
     color:white;
